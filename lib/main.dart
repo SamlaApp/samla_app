9fc06e671d0a739d9fcef0e41c367272e27d1002
Mohammed Alowa <47119751+mohsowa@users.noreply.github.com>
2023-10-13 12:15:21 +0300

    Edit Font, Edit pod file, in app notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'dart:io';
-
+import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:samla_app/core/auth/User.dart';
 import 'package:samla_app/core/error/exceptions.dart';
+import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 import 'config/router/app_router.dart';
 import 'firebase_options.dart'; // Import your logical code
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseInAppMessaging.instance.setAutomaticDataCollectionEnabled(true);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
 
   
   // final initialRoute = '';
@@ -31,6 +35,13 @@ class MyApp extends StatelessWidget {
           final mediaQueryData = MediaQuery.of(context);
           final scale = mediaQueryData.textScaleFactor
               .clamp(1.0, 1.4); // choose your max and min font sizes here
+          // disable rotation
+          if (mediaQueryData.orientation == Orientation.landscape) {
+            SystemChrome.setPreferredOrientations([
+              DeviceOrientation.portraitUp,
+              DeviceOrientation.portraitDown,
+            ]);
+          }
           return MediaQuery(
               data: MediaQuery.of(context).copyWith(textScaleFactor: scale),
               child: child!);
