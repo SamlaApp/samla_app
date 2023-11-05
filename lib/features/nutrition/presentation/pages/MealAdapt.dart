98c3fd393e3b47ccf9a93cbf8c7001b576bc988a
Mohammed Alowa <47119751+mohsowa@users.noreply.github.com>
2023-11-05 07:19:57 +0300

    some changes on MealAdapt

--- lib/features/nutrition/presentation/pages/MealAdapt.dart
+++ lib/features/nutrition/presentation/pages/MealAdapt.dart
@@ -7,52 +7,83 @@ import '../widgets/MaelAdapt/NutrientColumn.dart';
 import '../widgets/MaelAdapt/foodItem.dart';
 
 class MealAdapt extends StatelessWidget {
-  const MealAdapt({super.key, required NutritionPlanModel nutritionPlan});
+  final NutritionPlanModel nutritionPlan;
 
+  const MealAdapt({super.key, required this.nutritionPlan});
 
   @override
   Widget build(BuildContext context) {
+    String type = nutritionPlan.type;
+    late IconData icon;
+    late LinearGradient gradient;
+
+    if (type == 'Breakfast') {
+      icon = Icons.free_breakfast;
+      gradient = LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          theme_green,
+          Colors.blueAccent,
+        ],
+      );
+    } else if (type == 'Lunch') {
+      icon = Icons.lunch_dining;
+      gradient = LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [theme_orange, Colors.red],
+      );
+    } else if (type == 'Dinner') {
+      icon = Icons.dinner_dining;
+      gradient = LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [theme_darkblue, theme_green],
+      );
+    } else if (type == 'Snack') {
+      icon = Icons.fastfood;
+      gradient = LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          theme_pink,
+          Colors.blueAccent,
+        ],
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 200.0,
-        backgroundColor: theme_darkblue,
-
+        flexibleSpace: Container(
+          decoration: BoxDecoration(gradient: gradient),
+        ),
         title: Column(
           children: [
-            Icon(Icons.free_breakfast, size: 80),
-            SizedBox(height: 8),
-
-            SizedBox(height: 5),
-            Text('Breakfast Meal',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-
+            Icon(icon, size: 80),
+            const SizedBox(height: 5),
             Text(
-              '05:00am - 09:00am',
-              style: TextStyle(fontSize: 14),
+              nutritionPlan.name,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-
+            const SizedBox(height: 10),
+            Text(
+              '${nutritionPlan.start_time} - ${nutritionPlan.end_time}',
+              style: const TextStyle(fontSize: 14),
+            ),
           ],
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.share, size: 30),
-            onPressed: () {},
-          ),
-
-        ],
       ),
+
+
 
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-
               SizedBox(height: 20),
-
-
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
                 decoration: BoxDecoration(
@@ -73,15 +104,10 @@ class MealAdapt extends StatelessWidget {
                     NutrientColumn(value: '24', label: 'Protein'),
                     NutrientColumn(value: '18', label: 'Fat'),
                     NutrientColumn(value: '350', label: 'Total kcal'),
-
                   ],
                 ),
               ),
-
-
               SizedBox(height: 20),
-
-
               DayDropdown(
                 days: [
                   'Monday',
@@ -97,17 +123,11 @@ class MealAdapt extends StatelessWidget {
                   print("Selected day: $value");
                 },
               ),
-
-
               SizedBox(height: 20),
-              FoodItem(foodName: 'شاي حليب', kcal: 22, onRemove: () {
-              }),
-              FoodItem(foodName: 'كبدة', kcal: 213, onRemove: () {
-              }),
-              FoodItem(foodName: 'لحم بالطماط', kcal: 234, onRemove: () {
-              }),
-              FoodItem(foodName: 'بيض بالجبن', kcal: 187, onRemove: () {
-              }),
+              FoodItem(foodName: 'شاي حليب', kcal: 22, onRemove: () {}),
+              FoodItem(foodName: 'كبدة', kcal: 213, onRemove: () {}),
+              FoodItem(foodName: 'لحم بالطماط', kcal: 234, onRemove: () {}),
+              FoodItem(foodName: 'بيض بالجبن', kcal: 187, onRemove: () {}),
               SizedBox(height: 30),
             ],
           ),
