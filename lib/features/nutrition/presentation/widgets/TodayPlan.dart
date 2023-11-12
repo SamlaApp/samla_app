ac1fb80d78c0ad641e2a40ebe9d114512e32b606
AhmedAlhashim <103152298+AhmedAlhashim@users.noreply.github.com>
2023-11-12 19:05:23 +0300

    Nutrition UI improvments

--- lib/features/nutrition/presentation/widgets/TodayPlan.dart
+++ lib/features/nutrition/presentation/widgets/TodayPlan.dart
@@ -1,49 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:samla_app/config/themes/common_styles.dart';
+import 'package:intl/intl.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import '../../../../config/themes/common_styles.dart';
 import '../pages/NutritionPlan.dart';
-import 'package:samla_app/features/nutrition/presentation/cubit/NutritionPlan/nutritionPlan_cubit.dart';
-import 'package:samla_app/features/nutrition/nutrition_di.dart' as di;
 
 
-class TodayPlan extends StatelessWidget {
+class TodayPlan extends StatefulWidget {
+  @override
+  _TodayPlanState createState() => _TodayPlanState();
+}
+
+class _TodayPlanState extends State<TodayPlan> {
+  final PageController _pageController = PageController(viewportFraction: 0.85);
+
+  String _formatDate(DateTime dateTime) {
+    DateTime now = DateTime.now();
+    DateTime today = DateTime(now.year, now.month, now.day);
+    DateTime tomorrow = DateTime(now.year, now.month, now.day + 1);
+
+    if (dateTime.isAtSameMomentAs(today)) {
+      return 'Today';
+    } else if (dateTime.isAtSameMomentAs(tomorrow)) {
+      return 'Tomorrow';
+    } else {
+      return DateFormat('EEEE, MMM d').format(dateTime);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 225,
+      child: Stack(
+        children: [
+          PageView(
+            controller: _pageController,
+            children: [
+              for (int i = 0; i < 4; i++)
+                _dailyPlanCard(
+                  context,
+                  _formatDate(DateTime.now().add(Duration(days: i))),
+                ),
+            ],
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 15,
+            child: Center(
+              child: SmoothPageIndicator(
+                controller: _pageController,
+                count: 4,
+                effect: JumpingDotEffect(
+                  dotHeight: 10,
+                  dotWidth: 10,
+                  jumpScale: 2,
+                  activeDotColor: theme_green,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _dailyPlanCard(BuildContext context, String date) {
+    return Container(
+      margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
         gradient: background_gradient,
         borderRadius: primary_decoration.borderRadius,
         boxShadow: primary_decoration.boxShadow,
       ),
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(14.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Today Plan', style: textStyle.copyWith(fontSize: 20, color: Colors.white)),
+                Text(
+                  date,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
                 TextButton(
                   onPressed: () {
                     Navigator.of(context).push(MaterialPageRoute(builder: (context) => NutritionPlan()));
-
                   },
-                  child: Text('Edit', style: greyTextStyle.copyWith(fontSize: 14, color: Colors.white)),
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero,
+                  child: const Text(
+                    'Edit',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 10),
+            Divider(color: primary_color),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                foodItem('Egg', '30 kcal'),
-                foodItem('Milk', '30 kcal'),
-                foodItem('Bread', '30 kcal'),
-                foodItem('Orange', '30 kcal'),
+                _foodItem(context, Icons.egg, 'Egg', '30 kcal'),
+                _foodItem(context, Icons.coffee, 'Milk', '30 kcal'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _foodItem(context, Icons.breakfast_dining, 'Bread', '30 kcal'),
+                _foodItem(context, Icons.apple, 'Apple', '30 kcal'),
               ],
             ),
           ],
@@ -52,12 +127,37 @@ class TodayPlan extends StatelessWidget {
     );
   }
 
-  Column foodItem(String foodName, String calValue) {
+  Column _foodItem(BuildContext context, IconData icon, String foodName, String calValue) {
     return Column(
       children: [
-        Icon(Icons.breakfast_dining, color: theme_green, size: 30),
-        Text(foodName, style: greyTextStyle.copyWith(fontSize: 16, color: Colors.white)),
-        Text(calValue, style: greyTextStyle.copyWith(fontSize: 16, color: Colors.white)),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(icon, color: theme_green, size: 30),
+            SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  foodName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  calValue,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(height: 16),
       ],
     );
   }
