220e1187c1abbe794219a4aab064fe0ce978a348
= <atv@outlook.sa>
2023-11-18 01:55:27 +0300

    New UI style for the add tamplate

--- lib/features/training/presentation/pages/Tamplates_page.dart
+++ lib/features/training/presentation/pages/Tamplates_page.dart
@@ -77,79 +77,79 @@ class _TemplatesPageState extends State<TemplatesPage> {
     }
   }
 
-  void showExercisesDialog(String dayName, String exercises) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Dialog(
-          insetPadding: EdgeInsets.all(13),
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(dayName,
-                    style:
-                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
-                SizedBox(height: 20.0),
-                SingleChildScrollView(
-                  child: ListBody(
-                    children: exercises.split(', ').map((exercise) {
-                      return Card(
-                        clipBehavior: Clip.antiAlias,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: ListTile(
-                          leading: Padding(
-                            padding: const EdgeInsets.all(3.0),
-                            child: Image.network(
-                              'https://source.unsplash.com/featured/?gym',
-                              // Replace with your exercise image link
-                              width: 60,
-                              height: 60,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          title: Text(
-                            exercise, // The name of the exercise
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          trailing: IconButton(
-                            icon: Icon(Icons.info_outline),
-                            onPressed: () {
-                              // The functionality for showing exercise details goes here
-                              _showExerciseDetails(context,
-                                  exercise); // Make sure to define this method to show details
-                            },
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                TextButton(
-                  child: Text('Close'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
+  // void showExercisesDialog(String dayName, String exercises) {
+  //   showDialog(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return Dialog(
+  //         insetPadding: EdgeInsets.all(13),
+  //         child: Container(
+  //           width: MediaQuery.of(context).size.width * 0.9,
+  //           padding: EdgeInsets.all(20.0),
+  //           child: Column(
+  //             mainAxisSize: MainAxisSize.min,
+  //             children: <Widget>[
+  //               Text(dayName,
+  //                   style:
+  //                       TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+  //               SizedBox(height: 20.0),
+  //               SingleChildScrollView(
+  //                 child: ListBody(
+  //                   children: exercises.split(', ').map((exercise) {
+  //                     return Card(
+  //                       clipBehavior: Clip.antiAlias,
+  //                       shape: RoundedRectangleBorder(
+  //                         borderRadius: BorderRadius.circular(8),
+  //                       ),
+  //                       child: ListTile(
+  //                         leading: Padding(
+  //                           padding: const EdgeInsets.all(3.0),
+  //                           child: Image.network(
+  //                             'https://source.unsplash.com/featured/?gym',
+  //                             // Replace with your exercise image link
+  //                             width: 60,
+  //                             height: 60,
+  //                             fit: BoxFit.cover,
+  //                           ),
+  //                         ),
+  //                         title: Text(
+  //                           exercise, // The name of the exercise
+  //                           style: TextStyle(
+  //                             fontSize: 16,
+  //                             fontWeight: FontWeight.bold,
+  //                           ),
+  //                         ),
+  //                         trailing: IconButton(
+  //                           icon: Icon(Icons.info_outline),
+  //                           onPressed: () {
+  //                             // The functionality for showing exercise details goes here
+  //                             _showExerciseDetails(context,
+  //                                 exercise); // Make sure to define this method to show details
+  //                           },
+  //                         ),
+  //                       ),
+  //                     );
+  //                   }).toList(),
+  //                 ),
+  //               ),
+  //               SizedBox(height: 20.0),
+  //               TextButton(
+  //                 child: Text('Close'),
+  //                 onPressed: () {
+  //                   Navigator.of(context).pop();
+  //                 },
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
 
-  void _showExerciseDetails(BuildContext context, String exercise) {
-    // Implement your exercise details presentation logic here
-  }
+  // void _showExerciseDetails(BuildContext context, String exercise) {
+  //   // Implement your exercise details presentation logic here
+  // }
 
   ////////////////////////////  ////////////////////////////  ////////////////////////////
   @override
@@ -200,12 +200,13 @@ class _TemplatesPageState extends State<TemplatesPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            buildStartEmptyWorkoutButton(),
-            SizedBox(height: 16),
+            //
+            //buildStartEmptyWorkoutButton(),
+            // SizedBox(height: 16),
             buildTemplatesHeader(context),
-            SizedBox(height: 16),
-
-            Expanded(child: buildTemplateList(),),
+            // SizedBox(height: 16),
+            //
+            // Expanded(child: buildTemplateList(),),
             Expanded(child: buildBlocBuilder()),
           ],
         ),
@@ -215,36 +216,36 @@ class _TemplatesPageState extends State<TemplatesPage> {
 
   ////////////////////////////  ////////////////////////////  ////////////////////////////  ////////////////////////////
 
-  Widget buildStartEmptyWorkoutButton() {
-    return GestureDetector(
-      onTap: () {
-        // Implement the functionality for starting an empty workout
-      },
-      child: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          border: Border.all(color: theme_green, width: 2),
-          // Define your theme_green color in your theme
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Start Empty Workout',
-              style: TextStyle(
-                color: theme_green,
-                // Define your theme_green color in your theme
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
+  // Widget buildStartEmptyWorkoutButton() {
+  //   return GestureDetector(
+  //     onTap: () {
+  //       // Implement the functionality for starting an empty workout
+  //     },
+  //     child: Container(
+  //       padding: EdgeInsets.all(16),
+  //       decoration: BoxDecoration(
+  //         border: Border.all(color: theme_green, width: 2),
+  //         // Define your theme_green color in your theme
+  //         borderRadius: BorderRadius.circular(10),
+  //       ),
+  //       child: Row(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: [
+  //           Text(
+  //             'Start Empty Workout',
+  //             style: TextStyle(
+  //               color: theme_green,
+  //               // Define your theme_green color in your theme
+  //               fontSize: 18,
+  //               fontWeight: FontWeight.bold,
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+  //
   Widget buildTemplatesHeader(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -270,97 +271,97 @@ class _TemplatesPageState extends State<TemplatesPage> {
       ],
     );
   }
-
-  Widget buildTemplateList() {
-    return ListView.builder(
-      itemCount: templates.length,
-      itemBuilder: (context, index) {
-        final template = templates[index];
-        return buildTemplateCard(template);
-      },
-    );
-  }
-
-  Widget buildTemplateCard(Map<String, dynamic> template) {
-    String templateName = template['templateName'];
-    bool isTemplateAdded = addedTemplates[templateName] ?? false;
-
-    return Card(
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  template['templateName'] as String,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 16),
-                Container(
-                  height: 80,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: (template['days'] as List).map<Widget>((day) {
-                      return InkWell(
-                        onTap: () => showExercisesDialog(
-                          day['dayName'] as String,
-                          day['exercises'] as String,
-                        ),
-                        child: buildDayCard(day),
-                      );
-                    }).toList(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 8,
-            right: 8,
-            child: IconButton(
-              onPressed: () {
-                setState(() {
-                  addedTemplates[templateName] = !isTemplateAdded;
-                });
-              },
-              icon: Icon(
-                isTemplateAdded ? Icons.toggle_on : Icons.toggle_off,
-                size: 40,
-                color: isTemplateAdded ? theme_green : Colors.grey,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildDayCard(Map<String, dynamic> day) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              day['dayName'] as String,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(day['exercises'] as String),
-          ],
-        ),
-      ),
-    );
-  }
+  //
+  // Widget buildTemplateList() {
+  //   return ListView.builder(
+  //     itemCount: templates.length,
+  //     itemBuilder: (context, index) {
+  //       final template = templates[index];
+  //       return buildTemplateCard(template);
+  //     },
+  //   );
+  // }
+  //
+  // Widget buildTemplateCard(Map<String, dynamic> template) {
+  //   String templateName = template['templateName'];
+  //   bool isTemplateAdded = addedTemplates[templateName] ?? false;
+  //
+  //   return Card(
+  //     child: Stack(
+  //       children: [
+  //         Padding(
+  //           padding: const EdgeInsets.all(16.0),
+  //           child: Column(
+  //             crossAxisAlignment: CrossAxisAlignment.start,
+  //             children: [
+  //               Text(
+  //                 template['templateName'] as String,
+  //                 style: TextStyle(
+  //                   fontSize: 18,
+  //                   fontWeight: FontWeight.bold,
+  //                 ),
+  //               ),
+  //               SizedBox(height: 16),
+  //               Container(
+  //                 height: 80,
+  //                 child: ListView(
+  //                   scrollDirection: Axis.horizontal,
+  //                   children: (template['days'] as List).map<Widget>((day) {
+  //                     return InkWell(
+  //                       onTap: () => showExercisesDialog(
+  //                         day['dayName'] as String,
+  //                         day['exercises'] as String,
+  //                       ),
+  //                       child: buildDayCard(day),
+  //                     );
+  //                   }).toList(),
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //         Positioned(
+  //           top: 8,
+  //           right: 8,
+  //           child: IconButton(
+  //             onPressed: () {
+  //               setState(() {
+  //                 addedTemplates[templateName] = !isTemplateAdded;
+  //               });
+  //             },
+  //             icon: Icon(
+  //               isTemplateAdded ? Icons.toggle_on : Icons.toggle_off,
+  //               size: 40,
+  //               color: isTemplateAdded ? theme_green : Colors.grey,
+  //             ),
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+  //
+  // Widget buildDayCard(Map<String, dynamic> day) {
+  //   return Card(
+  //     child: Padding(
+  //       padding: const EdgeInsets.all(8.0),
+  //       child: Column(
+  //         crossAxisAlignment: CrossAxisAlignment.start,
+  //         children: [
+  //           Text(
+  //             day['dayName'] as String,
+  //             style: TextStyle(
+  //               fontSize: 16,
+  //               fontWeight: FontWeight.bold,
+  //             ),
+  //           ),
+  //           SizedBox(height: 8),
+  //           Text(day['exercises'] as String),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 
   // Pup up for new template, just name
   Widget buildNewTemplateDialog() {
