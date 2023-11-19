3ebacf5f62dafc8e7aaf783574c882a813e48e9b
AhmedAlhashim <103152298+AhmedAlhashim@users.noreply.github.com>
2023-11-19 21:06:51 +0300

    Nutrition UI improvments

--- lib/features/profile/presentation/widgets/AppBar_Widget01.dart
+++ lib/features/profile/presentation/widgets/AppBar_Widget01.dart
@@ -23,74 +23,23 @@ class buildAAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        padding: EdgeInsets.fromLTRB(25, 0, 15, 0),
-        height: preferredSize.height,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // row of profile
-            GestureDetector(
-              onTap: () => Navigator.pop(context),
-              child: Row(
-                children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage('images/Logo/1x/Icon_1@1x.png'),
-                      height: 50,
-                      width: 50,
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Text('SAMLAH',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
-                          decoration: TextDecoration.none,
-                          color: theme_darkblue.withOpacity(1))),
-                ],
-              ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Profile',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                  decoration: TextDecoration.none,
+                  color: theme_darkblue.withOpacity(1))),
+          backgroundColor: primary_color,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: theme_darkblue.withOpacity(0.5),
             ),
-            // row of upper buttons
-            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-              FloatingActionButton(
-                elevation: 0,
-                backgroundColor: Colors.transparent,
-                // Set background color to transparent
-                // foregroundColor: Colors.white,
-
-                onPressed: () {
-                  authBloc.add(LogOutEvent(context));
-                },
-                child: const Text(
-                  'Logout',
-                  softWrap: false,
-                  style: TextStyle(
-                    color: Color.fromRGBO(190, 30, 34, 1),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              IconButton(
-                  icon: SvgPicture.asset(
-                    'images/notification.svg',
-                    height: sizeOficon,
-                    color: iconColor,
-                  ),
-                  onPressed: () =>
-                      Navigator.pushNamed(context, '/Notifications')),
-              SizedBox(
-                width: 5,
-              ),
-              IconButton(
-                  icon: SvgPicture.asset(
-                    'images/qrcode.svg',
-                    height: sizeOficon,
-                    color: iconColor,
-                  ),
-                  onPressed: () => Navigator.pushNamed(context, '/QRcode')),
-            ])
-          ],
+            onPressed: () => Navigator.pop(context),
+          ),
         ),
       ),
     );
